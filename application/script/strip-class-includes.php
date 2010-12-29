d49823ee30a2df4cd9e9842007b85a19dbe9b078
Jason Paige <j@jasonpaige.co.uk>
2010-12-29 22:07:57 +0000

    Added some more test classes

---
+++ application/script/strip-class-includes.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Script to recursively strip out includes and requires of files which
+ * contain classes
+ * @todo this :)
+ */
+
---
+++ application/subfolder/TestClass2.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @description TestClass2
+ * @author jason
+ */
+class TestClass2 {
+
+    public static function isLoaded() {
+        return true;
+    }
+
+}
+
+class TestClass3 {
+
+    public static function isLoaded() {
+        return true;
+    }
+
+}
